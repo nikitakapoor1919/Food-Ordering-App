bc0dde62352c3852d711a52f758bed1ef2d3e29a
Nikita Kapoor <nikitakapoor1919@gmail.com>
2020-04-05 00:33:13 +0530

    protecting User Routes

--- routes/user.js
+++ routes/user.js
@@ -2,12 +2,28 @@ var express = require('express');
 var router = express.Router();
 var csrf = require('csurf');
 var passport = require('passport');
+var Product = require('../models/product');
 
 var csrfProtectionToken = csrf();
 router.use(csrfProtectionToken);
 
+router.get('/menu/:id',isLoggedIn,function(req,res){
+  username=req.user.email
+  Product.find({_id:req.params.id})
+  .exec(function(err,product){
+    if(err) console.log("Error")
+    else {
+      var ProductChunk = []; var chunkSize = 3;
+        for(var i=0; i < product.length; i += chunkSize){
+          ProductChunk.push(product.slice(i, i + chunkSize));
+        }
+        console.log(ProductChunk);
+        res.render('user/menu',{username:username,products:ProductChunk});
+    } 
+  })
 
-/* GET and POST : user signup page. */
+})
+/* GET and Product : user signup page. */
 router.get('/signup', function(req, res, next){
     var messages = req.flash('error');
     res.render('user/signup', { csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0 });
@@ -19,7 +35,7 @@ router.get('/signup', function(req, res, next){
     failureFlash: true
   }))
   
-  /* GET and POST : user signin page. */
+  /* GET and Product : user signin page. */
   router.get('/signin', function(req, res, next){
     var messages = req.flash('error');
     res.render('user/signin', { csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0 });
@@ -31,6 +47,26 @@ router.get('/signup', function(req, res, next){
     failureFlash: true
   }));
   
+  router.get('/logout', isLoggedIn, function(req, res, next){
+    req.logout();
+    res.redirect('/');
+  });
   
+  router.use('/', isNotLoggedIn, function(req,res, next){
+    next();
+  });
 
 module.exports = router;
+function isLoggedIn(req, res, next){
+  if(req.isAuthenticated()){
+    return next();
+  }
+  res.redirect('/user/signin');
+}
+
+function isNotLoggedIn(req, res, next){
+  if(!req.isAuthenticated()){
+    return next();
+  }
+  res.redirect('/');
+}

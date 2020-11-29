f0b8bfde4fc105ef88fb7969a4fb5001506ed783
Nikita Kapoor <nikitakapoor1919@gmail.com>
2020-11-30 02:38:37 +0530

    favicon added ,Layout improved, mongo atlas added

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ var AdminRoutes=require('./routes/admin')
 var UserRoutes=require('./routes/user')
 var routes = require('./routes/index');
 var app=express()
-
-mongoose.connect('mongodb://localhost:27017/FoodOrder', {useNewUrlParser: true});
-
+var uri="mongodb+srv://nikita:1998Nikita@1998@cluster0.4zyob.mongodb.net/FoodOrder?retryWrites=true&w=majority"
+// mongoose.connect('mongodb://localhost:27017/FoodOrder', {useNewUrlParser: true});
+mongoose.connect(uri,{useNewUrlParser: true, useCreateIndex: true });
 app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
